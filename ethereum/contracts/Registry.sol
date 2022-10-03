66857a527c9abc3fc1637b700798983876172159
ishtian <ishtian11@gmail.com>
2022-10-03 22:00:54 +0600

    - transferring awt tokens during buy property instead of native ETH added

--- ethereum/contracts/Registry.sol
+++ ethereum/contracts/Registry.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./Property.sol";
+import "./Token.sol";
 
 contract Registry {
 
@@ -11,6 +12,7 @@ contract Registry {
 
     Counters.Counter nProperties;
     Property property;
+    Token token;
     mapping(uint256 => PropertyInfo) public properties;
     PropertyInfo[] public propertyList;
     Purchase[] public purchases;
@@ -34,9 +36,10 @@ contract Registry {
     event BuyProperty(address buyer, uint256 pid, uint256 price);
     event PropertyAvailabilityEvent(uint256 id, bool available);
 
-    constructor(address _propertyContract) {
+    constructor(address _propertyContract, address _tokenAddress) {
         nProperties.reset();
         property = Property(_propertyContract);
+        token = Token(_tokenAddress);
     }
 
     function addProperty(uint256 _price, string memory _location, uint256 _size) public {
@@ -55,19 +58,28 @@ contract Registry {
         address propertyOwner = property.ownerOf(_pid);
         require(msg.sender != propertyOwner, "Property owner cannot buy their own property.");
         require(properties[_pid].isAvailable, "Currently this property is not available.");
-        require(msg.value == properties[_pid].price, "Not sufficient fund.");   // checking if buyer is sending exact price amount or not
 
-        (bool success, ) = address(propertyOwner).call{ value: msg.value }("");
-        require(success, "Failed to send money to the owner.");
+        uint256 propertyPrice = properties[_pid].price;
+
+        // this following codes is for transferring ETH
+        // require(msg.value == propertyPrice, "Not sufficient fund.");   // checking if buyer is sending exact price amount or not
+        // (bool success, ) = address(propertyOwner).call{ value: msg.value }("");
+        // require(success, "Failed to send money to the owner.");
+
+        // this following codes is for transferring AWT token
+        uint256 userBalance = token.balanceOf(msg.sender);
+        require(userBalance >= propertyPrice, "User does not have sufficient AWT tokens.");
+        (bool success) = token.transfer(msg.sender, propertyPrice);
+        require(success, "Failed to transfer token to user.");
 
         property.safeTransferFrom(propertyOwner, msg.sender, _pid);   // transferring the property token to the buyer
         properties[_pid].isAvailable = false;   // only after succsfull transaction setting the property is not available
         propertyList[_pid].isAvailable = false;
 
-        Purchase memory purchase = Purchase(_pid, msg.sender, propertyOwner, properties[_pid].price);
+        Purchase memory purchase = Purchase(_pid, msg.sender, propertyOwner, propertyPrice);
         purchases.push(purchase);   // pushing to the purchase array
 
-        emit BuyProperty(msg.sender, _pid, properties[_pid].price);
+        emit BuyProperty(msg.sender, _pid, propertyPrice);
     }
 
     function setPropertyAvailability(uint256 _pid, bool _status) public {
