55b71c4e16dd12dda73b2583be634072d84af89a
ishtian <ishtian11@gmail.com>
2022-10-06 13:59:20 +0600

    - output of compiled contracts are stroing into build directory

--- ethereum/migrations/04_registry_migration.js
+++ ethereum/migrations/04_registry_migration.js
@@ -1,6 +1,29 @@
+const path = require("path");
+const fs = require("fs-extra");
 const Registry = artifacts.require("Registry");
 const Property = artifacts.require("Property");
+const Vendor = artifacts.require("Vendor");
 const Token = artifacts.require("Token");
+
+const buildPath = path.resolve(__dirname, "build");
+fs.removeSync(buildPath);
+fs.ensureDirSync(buildPath);
+fs.outputJsonSync(
+  path.resolve(buildPath, "Token.json"),
+  JSON.parse(JSON.stringify(Token))
+);
+fs.outputJsonSync(
+  path.resolve(buildPath, "Vendor.json"),
+  JSON.parse(JSON.stringify(Vendor))
+);
+fs.outputJsonSync(
+  path.resolve(buildPath, "Property.json"),
+  JSON.parse(JSON.stringify(Property))
+);
+fs.outputJsonSync(
+  path.resolve(buildPath, "Registry.json"),
+  JSON.parse(JSON.stringify(Registry))
+);
 
 module.exports = function (deployer) {
   deployer.deploy(Registry, Property.address, Token.address);
