6a4681c57b82a752c4853941f99027f9e68d60a1
ishtian <ishtian11@gmail.com>
2022-10-06 12:57:35 +0600

    - web3.js file added

---
+++ ethereum/web3.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+import Web3 from "web3";
+
+let web3;
+
+if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+  // We are in the browser and metamask is running.
+  window.ethereum.request({ method: "eth_requestAccounts" });
+  web3 = new Web3(window.ethereum);
+} else {
+  // We are on the server *OR* the user is not running metamask
+  const provider = new Web3.providers.HttpProvider(process.env.INFURA_API_KEY);
+  web3 = new Web3(provider);
+}
+
+export default web3;
