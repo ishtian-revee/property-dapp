a2c4e0deb53793e731c206f7c0112d2cc252e0c0
ishtian <ishtian11@gmail.com>
2022-10-07 14:50:58 +0600

    - contracts deployed to goerli testnet

--- ethereum/truffle-config.js
+++ ethereum/truffle-config.js
@@ -89,7 +89,9 @@ module.exports = {
       network_id: 5,       // Goerli's id
       confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      skipDryRun: true,    // Skip dry run before migrations? (default: false for public nets )
+      networkCheckTimeout: 1000000,
+      from: process.env.DEPLOY_ACCOUNT
     },
     //
     // Useful for private networks
