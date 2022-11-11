628d0160dbe525ad2c6df8618577b1cc050239ac
ishtian <ishtian11@gmail.com>
2022-11-11 20:35:44 +0600

    - awt purchase row component added

---
+++ components/AWTPurchaseRow.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Table } from "semantic-ui-react";
+
+class AWTPurchaseRow extends Component {
+  render() {
+    const { Row, Cell } = Table;
+    const { type, buyer, amount, time } = this.props;
+
+    return (
+      <Row>
+        <Cell>{type}</Cell>
+        <Cell>{buyer}</Cell>
+        <Cell>{amount} AWT</Cell>
+        <Cell>{time}</Cell>
+      </Row>
+    );
+  }
+}
+
+export default AWTPurchaseRow;
